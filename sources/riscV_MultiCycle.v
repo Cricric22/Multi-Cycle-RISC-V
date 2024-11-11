f604c166b390c77e2a3dd6a56f682f2f0e16600e
Cricric22 <63693256+Cricric22@users.noreply.github.com>
2024-11-10 18:01:43 -0600

    Add files via upload

--- sources/riscV_MultiCycle.v
+++ sources/riscV_MultiCycle.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 10/23/2024 05:42:58 PM
+// Create Date: 10/23/2024 01:58:26 PM
 // Design Name: 
 // Module Name: riscV_MultiCycle
 // Project Name: 
@@ -20,38 +20,32 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module riscV_Multicycle(
-    input wire clk,
-    input wire reset,
-    input wire [31:0] ReadData,
-    output wire [31:0] Adr,
-    output wire MemWrite,
-    output wire [31:0] WriteData
+module riscV_MultiCycle (
+    input clk, reset,
+    input [31:0] ReadData,
+    output [31:0] Adr,
+    output MemWrite,
+    output [31:0] WriteData
 );
 
-wire [1:0] ResultSrc, ALUSrcA, ALUSrcB;
-wire [2:0] ImmSrc;
-wire [31:0] Instr;
-wire adrSrc, Zero, cout, overflow, sign;
-wire [3:0] alucontrol;
-wire irwrite, pcwrite;
-wire regwrite;
+    wire [1:0] ResultSrc, ALUSrcA, ALUSrcB;
+    wire [2:0] ImmSrc;
+    wire adrSrc, Zero, cout, overflow, sign;
+    wire [3:0] alucontrol;
+    wire irwrite, pcwrite;
+    wire regwrite;
+    wire [31:0] Instr;
 
-// Instancia del controlador
-controller c (
-    clk, reset, Instr[6:0], Instr[14:12], Instr[30], 
-    Zero, cout, overflow, sign, ImmSrc, ALUSrcA, ALUSrcB, 
-    ResultSrc, adrSrc, alucontrol, irwrite, pcwrite, regwrite, 
-    Memwrite
+    controller c (
+        clk, reset, Instr[6:0], Instr[14:12], Instr[30], 
+        Zero, cout, overflow, sign, ImmSrc, ALUSrcA, ALUSrcB, ResultSrc, adrSrc, 
+        alucontrol, irwrite, pcwrite, regwrite, MemWrite
     );
 
-// Instancia del datapath
-interconeccion dp (
-    clk, reset, ImmSrc, alucontrol, ResultSrc, 
-    irwrite, regwrite, ALUSrcA, ALUSrcB, adrSrc, 
-    pcwrite, ReadData, Zero, cout, overflow, sign, 
-    Adr, WriteData, Instr
+    interconeccion dp (
+        clk, reset, ImmSrc, alucontrol, ResultSrc, irwrite,
+        regwrite, ALUSrcA, ALUSrcB, adrSrc, pcwrite, 
+        ReadData, Zero, cout, overflow, sign, Adr, WriteData, Instr
     );
 
 endmodule
-
--- sources/top.v
+++ sources/top.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 10/23/2024 05:51:49 PM
+// Create Date: 10/23/2024 01:57:18 PM
 // Design Name: 
 // Module Name: top
 // Project Name: 
@@ -19,19 +19,34 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+
 module top(
-    input wire clk,
-    input wire reset,
-    output wire [31:0] WriteData,
-    output wire [31:0] DataAdr,
-    output wire MemWrite
+  input  clk,       // Señal de reloj
+  input  reset,     // Señal de reinicio
+  output [31:0] WriteData, // Dato que se escribirá en la memoria
+  output [31:0] DataAdr,   // Dirección de la memoria
+  output MemWrite   // Señal de escritura (1 = escritura, 0 = lectura)
 );
 
-wire [31:0] ReadData;
+  wire [31:0] ReadData; // Dato leído desde la memoria
 
-riscV_Multicycle rvMulti (clk, reset, ReadData, DataAdr, MemWrite, WriteData);
+  // Instancia del procesador RISC-V multiciclo
+  riscV_MultiCycle rvMulti(
+    clk, 
+    reset, 
+    ReadData, 
+    DataAdr, 
+    MemWrite, 
+    WriteData
+  );
 
-memory mem (clk, MemWrite, DataAdr, WriteData, ReadData);
+  // Instancia de la memoria
+  memory mem(
+    clk, 
+    MemWrite, 
+    DataAdr, 
+    WriteData, 
+    ReadData
+  );
 
 endmodule
-
